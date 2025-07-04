0221e016d647c0f61c94a3d50a0ac1db19738b34
samuelhenrique2008 <134607568+samuelhenrique2008@users.noreply.github.com>
2025-07-04 15:08:20 +0000

    sa

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
-let tamanhoSenha = 2;
+let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
 const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;    
+
+function diminuiTamanho(){
+if (tamanhoSenha > 1){
+    tamanhoSenha = tamanhoSenha-1;
+}
+tamanhoSenha = tamanhoSenha-1;
+numeroSenha.textContent = tamanhoSenha;
+
+
+}
+
+function aumentaTamanho(){
+tamanhoSenha = tamanhoSenha+1;
+numeroSenha.textContent = tamanhoSenha;
+}
 
 console.log(botoes)
